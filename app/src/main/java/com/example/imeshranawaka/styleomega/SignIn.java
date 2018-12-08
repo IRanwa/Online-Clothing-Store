deb5c7fbcf0ebc711a9f58c89ea68b6ad935003d
imesh <imeshranwa2@gotmail.com>
2018-12-08 20:00:08 +0530

    Created Fragment Register Updated Fragment Sign In

--- app/src/main/java/com/example/imeshranawaka/styleomega/SignIn.java
+++ app/src/main/java/com/example/imeshranawaka/styleomega/SignIn.java
@@ -2,17 +2,12 @@ package com.example.imeshranawaka.styleomega;
 
 
 import android.os.Bundle;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,7 +24,7 @@ public class SignIn extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-         v = inflater.inflate(R.layout.activity_sign_in, container, false);
+         v = inflater.inflate(R.layout.fragment_sign_in, container, false);
         // Inflate the layout for this fragment
 
         v.findViewById(R.id.btnRegister).setOnClickListener(new registerNow_onClick());
